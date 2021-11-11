0e9ec8b5be1bb5e36b6d3d8684db959845d47fc5
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-11 11:06:07 +0530

    Employee Part Time Added

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -2,16 +2,22 @@ package com.employee;
 
 public class Employee {
 	public static void main(String[] args) {
-		double attendance = Math.floor(Math.random()*2);
-		int wages,ratePerHour=20,empHrs=8; 
-        System.out.println(attendance);
-        if(attendance == 1) {
-	   		wages=ratePerHour*empHrs;
-	   		System.out.println("Employee is present \nwages = " + wages);
-        }else {
-	        wages=0;
-	   		System.out.println("Employee is absent \nwages = " + wages);
-        }
+		double attendance = Math.floor(Math.random()*3);
+		int ratePerHour=20;
+		int empHrsFull = 8;
+		int empHrsPartTime= 4;
+		if(attendance==1) {
+			System.out.println("FullTime Wage");
+			System.out.println( ratePerHour * empHrsFull );
+		}
+		else if(attendance==2) {
+			System.out.println("PartTime Wage");
+			System.out.println( ratePerHour * empHrsPartTime);
+		}
+		else {
+			System.out.println("Absent");
+			System.out.println("Salary = 0");
+		}
 	}
 }
 
