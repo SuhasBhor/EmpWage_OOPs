d2d7695901b2c72fae9ed5d62494d5e800ad4e8b
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-11 11:13:55 +0530

    EmpWage for Month

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,22 +1,28 @@
 package com.employee;
 
 import java.util.Random;
-
 public class Employee {
 	public static void main(String[] args) {
-		int ratePerHour=20,empHrsFull=8,empHrsPartTime=4;
-		Random rd = new Random();
-		int empcheck = rd.nextInt(3);
-		switch(empcheck) {
-		case 1:
-			System.out.println("Salary of Fulltime Employee:" + ratePerHour * empHrsFull);
-		break;
-		case 2:
-			System.out.println("Salary of Parttime Employee:" + ratePerHour * empHrsPartTime );
-		break;
-		default:
-			System.out.println("Employee is Absent Salary: 0");
+		int ratePerHour=20,empHrs=0,totalWorkingHrs=0,salary=0;
+		int i,workingDays=20;
+		for(i=1;i<=workingDays;i++) {
+			Random rd = new Random();
+			int empcheck = rd.nextInt(3);
+			switch(empcheck) {
+				case 1:
+					System.out.println(empHrs=8);
+					break;
+				case 2:
+					System.out.println(empHrs=4);
+					break;
+				default:
+					System.out.println(empHrs=0);
+			}
+			System.out.println(totalWorkingHrs=totalWorkingHrs + empHrs);
 		}
+		System.out.println("Total Working Hour:" +totalWorkingHrs);
+		System.out.println("Total Salary:");
+		System.out.println(salary=totalWorkingHrs * ratePerHour);
 	}
 }
 
