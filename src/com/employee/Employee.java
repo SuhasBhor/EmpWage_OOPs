5bef21b055631392e39311ad3081de5e04290c90
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-11 11:03:41 +0530

    Employee Daily Wage

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,15 +1,17 @@
 package com.employee;
 
 public class Employee {
-		public static void main(String[] args) {
-			double attendance =Math.floor(Math.random()*2);
-	        System.out.println(attendance);
-	          if(attendance == 1) {
-	    	   System.out.println("Employee is present");
-	          }
-	          else {
-	    	   System.out.println("Employee is absent");
-	          }
-	     }
+	public static void main(String[] args) {
+		double attendance = Math.floor(Math.random()*2);
+		int wages,ratePerHour=20,empHrs=8; 
+        System.out.println(attendance);
+        if(attendance == 1) {
+	   		wages=ratePerHour*empHrs;
+	   		System.out.println("Employee is present \nwages = " + wages);
+        }else {
+	        wages=0;
+	   		System.out.println("Employee is absent \nwages = " + wages);
+        }
+	}
 }
 
