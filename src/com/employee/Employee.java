03b00f8418f6be6e4159b30a8314287944dc3e27
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-11 11:11:00 +0530

    Employee Wage Using Switch Case

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,22 +1,21 @@
 package com.employee;
 
+import java.util.Random;
+
 public class Employee {
 	public static void main(String[] args) {
-		double attendance = Math.floor(Math.random()*3);
-		int ratePerHour=20;
-		int empHrsFull = 8;
-		int empHrsPartTime= 4;
-		if(attendance==1) {
-			System.out.println("FullTime Wage");
-			System.out.println( ratePerHour * empHrsFull );
-		}
-		else if(attendance==2) {
-			System.out.println("PartTime Wage");
-			System.out.println( ratePerHour * empHrsPartTime);
-		}
-		else {
-			System.out.println("Absent");
-			System.out.println("Salary = 0");
+		int ratePerHour=20,empHrsFull=8,empHrsPartTime=4;
+		Random rd = new Random();
+		int empcheck = rd.nextInt(3);
+		switch(empcheck) {
+		case 1:
+			System.out.println("Salary of Fulltime Employee:" + ratePerHour * empHrsFull);
+		break;
+		case 2:
+			System.out.println("Salary of Parttime Employee:" + ratePerHour * empHrsPartTime );
+		break;
+		default:
+			System.out.println("Employee is Absent Salary: 0");
 		}
 	}
 }
