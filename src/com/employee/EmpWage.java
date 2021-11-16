77a3c9bb2032e283d0d42be45b99fa6effe61809
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-16 16:17:55 +0530

    Calculate EmpWage of Different Companies

--- src/com/employee/EmpWage.java
+++ src/com/employee/EmpWage.java
@@ -5,26 +5,13 @@ import java.util.Scanner;
 public class EmpWage {
 	public static void main(String[] args) {
 		Employee emp = new Employee();
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Enter How Many Companies Wage You Want To Calculate: ");
-		int n = scan.nextInt();
+		emp.computeEmpWage("TCS",20,120,40);
 		
-		for(int i=0;i<n;i++) {
-			scan.nextLine();
-			System.out.print("Enter Company Name: ");
-			String name = scan.nextLine();
-			
-			System.out.print("Enter Max Working Days of Company in Month: ");
-			int No_Of_Working_Days = scan.nextInt();
-			
-			System.out.print("Enter Max Working Hour of Company: ");
-			int Emp_Max_Hours = scan.nextInt();
-			
-			System.out.print("Enter Wage Per Hour: ");
-			int Emp_Rate = scan.nextInt();
-			
-			emp.computeEmpWage(name,No_Of_Working_Days,Emp_Max_Hours,Emp_Rate);	
-		}
+		emp.computeEmpWage("Infosys", 20, 110, 45);
+		
+		emp.computeEmpWage("Tech-Mahindra", 20, 100, 50);
 	}
 }
+
+
