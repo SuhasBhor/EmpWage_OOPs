a9f53edc446a0067f8747bd293ca9b182bc337cd
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-17 19:51:40 +0530

    Store Monthly EmpWage Of Companies Along With DailyEmpWage

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,5 +1,6 @@
 package com.employee;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Employee implements EmpWageBuilder{
@@ -7,13 +8,15 @@ public class Employee implements EmpWageBuilder{
 	final int isFullTime = 2;
     final int isPartTime = 1;
     int totalEmpWage;
-  
+    
+    ArrayList<Integer> dailyEmpWageArray = new ArrayList<Integer>();
     public void computeEmpWage(CompanyEmpWage companyEmpWage) {
       int empHrs = 0;
       int empWage = 0;
       int totalEmpHrs = 0;
       int numOfDays = 0;
-
+      int dailyEmpWage=0;
+      
       while (totalEmpHrs <= companyEmpWage.Max_Working_Hours && numOfDays < companyEmpWage.Total_Working_Days ) {
          numOfDays++;
          int empCheck = (int) Math.floor(Math.random()*3);
@@ -28,9 +31,12 @@ public class Employee implements EmpWageBuilder{
                empHrs = 0;
             }
          totalEmpHrs += empHrs;
+         dailyEmpWage = totalEmpHrs * companyEmpWage.Emp_Rate;
+         dailyEmpWageArray.add(dailyEmpWage);
+         System.out.println("Day "+numOfDays+ " EmpWage is "+dailyEmpWage);
         }
       companyEmpWage.setTotal_Emp_Wage(totalEmpHrs* companyEmpWage.Emp_Rate);
-      System.out.println("Employee Monthly Wage of Company "+companyEmpWage.name+ " is "+companyEmpWage.Total_Emp_Wage);
+      System.out.println("\nEmployee Monthly Wage of Company "+companyEmpWage.name+ " is "+companyEmpWage.Total_Emp_Wage+"\n");
    }
 }
 
